8db671b430d5ef2e38fe41bd10d143644237b274
BMarx981 <b.marx981@gmail.com>
2019-08-19 20:44:10 -0400

    commit to switch branches to committee

--- lib/screens/committees_screen.dart
+++ lib/screens/committees_screen.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:seatd/network_helper.dart';
 
 class CommitteesScreen extends StatefulWidget {
-  CommitteesScreen({Key key, @required this.chamber}) : super(key: key);
+  CommitteesScreen(
+      {Key key, @required this.chamber, @required this.congressNum})
+      : super(key: key);
   final String chamber;
+  final String congressNum;
   static const String id = 'committes';
   @override
   _CommitteesScreenState createState() => _CommitteesScreenState();
@@ -14,7 +17,7 @@ class _CommitteesScreenState extends State<CommitteesScreen> {
 
   String _getTitle() {
     if (widget.chamber == 'Joint') {
-      return 'Joint Statements';
+      return 'Joint Commitees';
     }
     return 'Commitees for the ${widget.chamber}';
   }
